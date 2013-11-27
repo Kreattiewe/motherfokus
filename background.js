f6439bac3f5256dbf4250d1cbf7a4bdefa30f7f5
Angel Botto <angelbotto@gmail.com>
2013-11-27 03:23:27 -0500

    Init motherfokus

---
+++ background.js
@@ -0,0 +1,16 @@
+/*
+
+Kreattiewe Inc - 2013
+
+El equipo de desarrolladores de Kreattiewe estaba cansado de perder tiempo
+en facebook, asi que creamos esta pendejada para simplemente bloquear la pagina 
+principal de facebook, y enfocarnos en programar.
+
+Programming, Motherfucker
+Do you speak it?
+http://programming-motherfucker.com/
+
+*/
+if (window.location.hostname == "www.facebook.com") {
+	window.location = 'http://kreattiewe.co/motherfokus';
+}
